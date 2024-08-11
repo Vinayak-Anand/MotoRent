1c36c31b88b7af26bdb89102d9b3259e0921b65d
Vinayak Anand <vinayakanand010503@gmail.com>
2024-08-11 10:24:11 +0530

    implement carousel and card design

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+import Carousels from "@/components/Carousel";
+import Explore from "@/components/Explore";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container">Home Page </div>
+    <div className="">
+      <Carousels /> 
+      <div className="relative">
+        <Explore />
+      </div>
+      </div>
   );
 }
